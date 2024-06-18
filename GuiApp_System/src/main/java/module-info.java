ba7cf27cf4707c660c9f573fc21b3e6d3461a784
Bruno Kennedy <bruno.klqueiroz@gmail.com>
2024-06-18 11:48:44 +0100

    adding the local respository online

---
+++ GuiApp_System/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.guiapp_system {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.guiapp_system to javafx.fxml;
+    exports org.example.guiapp_system;
+}
